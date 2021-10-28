68503749c817949971d16b9398ac114a7d3fc907
vlnt1 <92628033+vlnt1@users.noreply.github.com>
2021-10-28 18:05:46 +0700

    Add files via upload

---
+++ latihan.py
@@ -0,0 +1,20 @@
+print("hello")
+print("saya sedang belajar python")
+
+a = 8
+b = 6
+print("variabel a=",a)
+print("variabel b=",b)
+print("hasil penjumblahan a+b=",a+b)
+
+a=input("masukan nilai a:")
+b=input("masukan nilai b")
+print("variabel a=",a)
+print("variabel b=",b)
+print("hasil penggabungan {1}&{0}=%d".format(a,b) %(a+b))
+
+#konfrensi nilai variabel
+a=int(a)
+b=int(b)
+print("hasil penjumblahan {1}+{0}=%d".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
